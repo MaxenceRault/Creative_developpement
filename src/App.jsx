bdd8c9cecd665015dd2a34657cb4800a5c7fd6bd
Maxence Rault <raultmaxence05@gmail.com>
2025-03-12 12:54:03 +0100

    merge calme et mystere

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,17 @@ import React from "react";
 
 import Calme from "./components/calme";
 import Intro from "./components/intro";
-
+import Mystere from "./components/mystere";
 
 
 const App = () => {
-    
+    return (
+        <>
+            <Intro />
+            <Calme />
+            <Mystere />
+        </>
+    );
 };
 
 export default App;
